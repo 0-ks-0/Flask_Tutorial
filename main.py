7ac30c8958bff58309fe86dbb350767714f4c9d9
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-04-15 17:17:01 -0400

    Removed unnecessary things. Added management page

--- main.py
+++ main.py
@@ -10,48 +10,44 @@ conn = engine.connect()
 # render a file
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-
-# remember how to take user inputs?
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
-
+	return render_template('index.html')
 
 # get all boats
 # this is done to handle requests for two routes -
 @app.route('/boats/')
 @app.route('/boats/<page>')
 def get_boats(page=1):
-    page = int(page)  # request params always come as strings. So type conversion is necessary.
-    per_page = 10  # records to show per page
-    boats = conn.execute(text(f"SELECT * FROM boats LIMIT {per_page} OFFSET {(page - 1) * per_page}")).all()
-    print(boats)
-    return render_template('boats.html', boats=boats, page=page, per_page=per_page)
+	page = int(page)  # request params always come as strings. So type conversion is necessary.
+	per_page = 10  # records to show per page
+	boats = conn.execute(text(f"SELECT * FROM boats LIMIT {per_page} OFFSET {(page - 1) * per_page}")).all()
+	print(boats)
+	return render_template('boats.html', boats=boats, page=page, per_page=per_page)
 
+@app.route('/manage')
+def navigate_to_manage():
+	return render_template('manage.html')
 
-@app.route('/create', methods=['GET'])
+@app.route('/manage/create', methods=['GET'])
 def create_get_request():
-    return render_template('boats_create.html')
+	return render_template('boats_create.html')
 
 
-@app.route('/create', methods=['POST'])
+@app.route('/manage/create', methods=['POST'])
 def create_boat():
-    # you can access the values with request.from.name
-    # this name is the value of the name attribute in HTML form's input element
-    # ex: print(request.form['id'])
-    try:
-        conn.execute(
-            text("INSERT INTO boats values (:id, :name, :type, :owner_id, :rental_price)"),
-            request.form
-        )
-        return render_template('boats_create.html', error=None, success="Data inserted successfully!")
-    except Exception as e:
-        error = e.orig.args[1]
-        print(error)
-        return render_template('boats_create.html', error=error, success=None)
+	# you can access the values with request.from.name
+	# this name is the value of the name attribute in HTML form's input element
+	# ex: print(request.form['id'])
+	try:
+		conn.execute(
+			text("INSERT INTO boats values (:id, :name, :type, :owner_id, :rental_price)"),
+			request.form
+		)
+		return render_template('boats_create.html', error=None, success="Data inserted successfully!")
+	except Exception as e:
+		error = e.orig.args[1]
+		print(error)
+		return render_template('boats_create.html', error=error, success=None)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
